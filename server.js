0c3f54959a2ae44ee56eae3e819313d386cfbded
Gabriele <163661082+GabryC0de@users.noreply.github.com>
2024-09-29 17:17:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ async function run() {
         app.get('/', (req, res) => {
           res.sendFile(__dirname + '/index.html');
         });
+        app.use(express.static('/'));
         app.post('/register', async (req, res) => {
             if (!db) {
                 return res.status(500).json({ message: 'Database not connected' });
